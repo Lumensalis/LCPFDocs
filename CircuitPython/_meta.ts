ef9cb3f9446b0a76d2adb4dc915c897c11f3ebb3
James Fowler <james@openseaconsulting.com>
2025-06-27 13:34:09 -0400

    added quirks.mdx

---
+++ CircuitPython/_meta.ts
@@ -0,0 +1,11 @@
+import type { MetaRecord } from 'nextra';
+
+const meta : MetaRecord = {
+    //'index' : {    },
+    'CIRCUITPY_Dependncies': {  },
+    'ConCIRCUITPY_DRIVE': {  },
+    //'Behaviors': { title: 'Behaviors' },
+    "Quirks": { },
+};
+
+export default meta;
