251cdd6fa1f680c16bb3a374670d6c26fc76449e
James Fowler <james@openseaconsulting.com>
2025-06-27 11:35:00 -0400

    restructure doc directories

---
+++ GettingStarted/_meta.ts
@@ -0,0 +1,10 @@
+import type { MetaRecord } from 'nextra';
+
+const meta : MetaRecord = {
+    //'index' : {    },
+    'S2MiniQuickStart': { },
+    //'Behaviors': { title: 'Behaviors' },
+    "Soldering": { },
+};
+
+export default meta;
---
+++ _meta.ts
@@ -0,0 +1,13 @@
+import type { MetaRecord } from 'nextra';
+
+const meta : MetaRecord = {
+    //'index' : {    },
+    'GettingStarted': { },
+    'Concepts': {  },
+    //'Behaviors': { title: 'Behaviors' },
+    "History": { },
+    "RepoContents": {"display": "hidden"},
+    "Dev": {}, // { "display": "hidden"} ,
+};
+
+export default meta;
